d204638f822361f77530be539da4880341a10431
飄然偌僊 <13104871646@163.com>
2021-01-15 17:35:28 +0800

    build：新建项目；

---
+++ app/src/main/java/com/fphoenixcorneae/core/demo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.fphoenixcorneae.core.demo
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
