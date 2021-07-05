10628d2a45eb6d533334d14c1dd3f688efeba92b
FPhoenixCorneaE <13104871646@163.com>
2021-07-05 10:48:18 +0800

    fix:自定义 attr 在 xml 没有提示问题; build:组件版本升级为 1.1.0;

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -35,8 +35,8 @@ object Deps {
         const val buildToolsVersion = "30.0.3"
         const val minSdkVersion = 21
         const val targetSdkVersion = 30
-        const val versionCode = 109
-        const val versionName = "1.0.9"
+        const val versionCode = 110
+        const val versionName = "1.1.0"
         const val kotlinVersion = "1.5.10"
     }
 
