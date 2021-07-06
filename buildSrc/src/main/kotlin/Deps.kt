e4c077a510a6beb2927580fc21108b2e9c20d4a7
FPhoenixCorneaE <13104871646@163.com>
2021-07-06 14:50:47 +0800

    build:组件版本升级为 1.1.4;

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -34,8 +34,8 @@ object Deps {
         const val buildToolsVersion = "30.0.3"
         const val minSdkVersion = 21
         const val targetSdkVersion = 30
-        const val versionCode = 113
-        const val versionName = "1.1.3"
+        const val versionCode = 114
+        const val versionName = "1.1.4"
         const val kotlinVersion = "1.5.10"
     }
 
