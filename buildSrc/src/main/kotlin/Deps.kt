1684749609101036da61c8e39a993f4517d3624c
FPhoenixCorneaE <13104871646@163.com>
2021-08-02 17:18:31 +0800

    build:组件版本升级为 1.1.8; rm:jm_attrs.xml;

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -34,8 +34,8 @@ object Deps {
         const val buildToolsVersion = "30.0.3"
         const val minSdkVersion = 21
         const val targetSdkVersion = 30
-        const val versionCode = 117
-        const val versionName = "1.1.7"
+        const val versionCode = 118
+        const val versionName = "1.1.8"
         const val kotlinVersion = "1.5.20"
     }
 
