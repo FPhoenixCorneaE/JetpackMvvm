70f4fffc9ef934f8b6c5bcf8653170200226efe4
FPhoenixCorneaE <13104871646@163.com>
2022-07-28 14:02:03 +0800

    feat:databinding-RecyclerView::"hasFixedSize", "clipToPadding", "itemViewCacheSize", "itemAnimator";

--- jetpackMvvm/src/main/kotlin/com/fphoenixcorneae/jetpackmvvm/databinding/RecyclerViewBindingAdapter.kt
+++ jetpackMvvm/src/main/kotlin/com/fphoenixcorneae/jetpackmvvm/databinding/RecyclerViewBindingAdapter.kt
@@ -1,29 +1,34 @@
 package com.fphoenixcorneae.jetpackmvvm.databinding
 
 import androidx.databinding.BindingAdapter
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.fphoenixcorneae.jetpackmvvm.listener.OnLoadMoreListener
 import com.fphoenixcorneae.jetpackmvvm.listener.OnScrollLoadMoreListener
 
-@BindingAdapter(value = ["adapter", "manager"], requireAll = false)
+@BindingAdapter(value = [
+    "adapter", "manager", "hasFixedSize", "clipToPadding", "itemViewCacheSize", "itemAnimator",
+], requireAll = false)
 fun initRecycler(
     recyclerView: RecyclerView,
-    adapter: RecyclerView.Adapter<*>? = null,
-    layoutManager: RecyclerView.LayoutManager? = LinearLayoutManager(recyclerView.context),
+    adapter: RecyclerView.Adapter<*>?,
+    layoutManager: RecyclerView.LayoutManager?,
+    hasFixedSize: Boolean?,
+    clipToPadding: Boolean?,
+    itemViewCacheSize: Int?,
+    itemAnimator: RecyclerView.ItemAnimator?,
 ) {
-    adapter?.let {
-        recyclerView.adapter = it
-    }
-    layoutManager?.let {
-        recyclerView.layoutManager = it
-    }
+    adapter?.let { recyclerView.adapter = it }
+    layoutManager?.let { recyclerView.layoutManager = it }
+    hasFixedSize?.let { recyclerView.setHasFixedSize(it) }
+    clipToPadding?.let { recyclerView.clipToPadding = it }
+    itemViewCacheSize?.let { recyclerView.setItemViewCacheSize(it) }
+    itemAnimator?.let { recyclerView.itemAnimator = it }
 }
 
 @BindingAdapter(value = ["onLoadMore"], requireAll = false)
 fun setOnLoadMoreListener(
     recyclerView: RecyclerView,
-    onLoadMoreListener: OnLoadMoreListener
+    onLoadMoreListener: OnLoadMoreListener,
 ) {
     recyclerView.addOnScrollListener(OnScrollLoadMoreListener(onLoadMoreListener = onLoadMoreListener))
 }
