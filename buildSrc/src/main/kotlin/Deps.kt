35ddb3f07b2fb9a315a047bf92c3b0f4e253f850
FPhoenixCorneaE <13104871646@163.com>
2022-08-08 15:12:49 +0800

    build: component upgrade to 2.0.4

--- buildSrc/src/main/kotlin/Deps.kt
+++ buildSrc/src/main/kotlin/Deps.kt
@@ -34,8 +34,8 @@ object Deps {
         const val buildToolsVersion = "32.0.0"
         const val minSdk = 21
         const val targetSdk = 32
-        const val versionCode = 203
-        const val versionName = "2.0.3"
+        const val versionCode = 204
+        const val versionName = "2.0.4"
         const val testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
